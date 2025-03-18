b3f20adee9fe3c412523762a3d345e102bfa3db9
Felipe0Freitas0B <felipe0f3l1p3@gmail.com>
2025-03-18 20:07:17 -0300

    Add files via upload

---
+++ ParImpar.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ParImpar {
+    public static void main(String[] args){
+
+        Scanner entrada = new Scanner(System.in); int num;
+
+        System.out.println("Digite um número inteiro: ");
+        num = entrada.nextInt();
+
+        if((num % 2.0)==0){
+            System.out.println(num + " é par.");
+        }else{
+            System.out.println(num + " é ímpar.");
+        }
+        entrada.close(); 
+    }
+   
+}
+
