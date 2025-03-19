671a8b856c1cb88d43e147d08d07bf2cfe8155fc
Felipe0Freitas0B <felipe0f3l1p3@gmail.com>
2025-03-18 21:32:36 -0300

    Add files via upload

---
+++ menu.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class menu {
+    public static void main(String[] args){
+        
+        System.out.println("<======== Menu de Opções ========>");
+        System.out.println(" 1 - Cadastrar Produtos");
+        System.out.println(" 2 - Lista de Produtos");
+        System.out.println(" 3 - Sair do Sistema");
+        System.out.println("<====== Escolha uma Opção =======>");
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        int menu = entrada.nextInt();
+        
+        switch(menu){
+            case 1:
+                System.out.println("Voce escolheu 1");
+                System.out.println("Cadastrar Produtos");
+                break;
+            
+            case 2: 
+                System.out.println("Voce escolheu 2");
+                System.out.println("Lista de Produtos");
+                break;
+                
+            case 3:
+                System.out.println("Voce escolheu 3");
+                System.out.println("Sair do Sistema");
+                break;
+                
+            default:
+                System.out.println("Invalido");
+        }
+            entrada.close();
+    }
+}
+ 
+
